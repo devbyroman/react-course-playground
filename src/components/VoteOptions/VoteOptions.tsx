4ced4e8cfd8f667f74f2e7fa3b7a969b32ca10a2
Roman <roman.martyniakdev@gmail.com>
2025-08-28 19:30:55 +0300

    complete 02-react-cafe

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,27 @@
+import css from './VoteOptions.module.css';
+import { type VoteButtons } from './VoteOptions';
+
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteButtons) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote('good')}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote('neutral')}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote('bad')}>
+        Bad
+      </button>
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
